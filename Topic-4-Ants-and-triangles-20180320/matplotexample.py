9872f48eff657d6b9deeaecfc90eeeda1a775f77
Ringo <zcabswc@ucl.ac.uk>
2018-07-07 01:50:06 +0100

    Recreate dict

--- Topic-4-Ants-and-triangles-20180320/matplotexample.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pylab as plt
-
-plt.plot([0,1,2],[3,6,2])
-plt.xlabel("Some numbers")
-plt.ylabel("Other numbers")
-# all other plotting stuff goes here
-
-plt.savefig("example.png")
